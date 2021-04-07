6c3c4fc0e50e83c7354998ed0ea4d16a0b89cbe5
caster-au <as0944868@gmail.com>
2021-04-07 09:15:02 +0530

    data type

---
+++ DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+    public static void main(String args[])
+    {
+    int a=10,A=20;
+    System.out.println(" "+(a+A));
+    float b=10.5f,B=20.5f;
+    System.out.println(" "+(b+B));
+    double c=10.55,C=20.55;
+    System.out.println(" "+(c+C));
+    }
+}
+
