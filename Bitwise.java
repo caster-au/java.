409c587709ce8e48b65d872bc232e2ce46efeedb
caster-au <as0944868@gmail.com>
2021-04-09 09:32:17 +0530

    bitwise operator

---
+++ Bitwise.java
@@ -0,0 +1,9 @@
+public class Bitwise {
+    public static void main(String args[])
+    {
+        int a= 10 & 7;
+        System.out.println(a);
+        int b= 12|9;
+        System.out.println(b);
+    }
+}
