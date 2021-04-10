61f2966aab17abc3d09a2b0a36e095fd6b12ba48
caster-au <as0944868@gmail.com>
2021-04-10 19:38:41 +0530

    boolean data

---
+++ Boolean.java
@@ -0,0 +1,9 @@
+public class Boolean{
+    public static void main(String args[])
+    {
+        int a=10,b=10,c=10;
+        boolean x;
+        x= a == b && b == c;
+        System.out.println(x);
+    }
+}
