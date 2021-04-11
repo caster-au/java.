e467b9a60fd04d604841892efc3a5d90cf96213f
caster-au <as0944868@gmail.com>
2021-04-11 09:09:14 +0530

    compound operator

---
+++ Compound.java
@@ -0,0 +1,10 @@
+public class Compound {
+    public static void main(String args[])
+    {
+        int a=15;
+        a+=a;
+        System.out.println(a);
+        a*=a;
+        System.out.println(a);
+    }
+}
