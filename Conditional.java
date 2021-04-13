84f4175ac953ca747ece03b9f762ef87b83bab6e
caster-au <as0944868@gmail.com>
2021-04-13 18:09:00 +0530

    conditional statment

---
+++ Conditional.java
@@ -0,0 +1,14 @@
+public class Conditional {
+    public static void main(String args[])
+    {
+        int a=10;
+        if(a>10)
+        {
+            System.out.println("greater");
+        }
+        else{
+            System.out.println("equal");
+        }
+        System.out.println("bye");
+    }
+}
