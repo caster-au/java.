f4534bf2220e89cf108fd8e642e806c58efd78ae
caster-au <as0944868@gmail.com>
2021-04-15 12:39:41 +0530

    Switch case

---
+++ Switch.java
@@ -0,0 +1,27 @@
+public class Switch {
+    public static void main(String args[])
+    {
+        int a=58;
+        switch(a)
+        {
+            case 1:
+            System.out.println("one");
+            break;
+            case 2:
+            System.out.println("two");
+            break;
+            case 3:
+            System.out.println("three");
+            break;
+            case 4:
+            System.out.println("four");
+            break;
+            case 5:
+            System.out.println("five");
+            break;
+            default:
+            System.out.println("invalid not in range of 1 to 5");
+            break;
+        }
+    }
+}
