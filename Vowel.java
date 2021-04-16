35fed6e175df93f592b796f462dc01428e19e648
caster-au <as0944868@gmail.com>
2021-04-16 20:42:05 +0530

    switch

---
+++ Vowel.java
@@ -0,0 +1,18 @@
+public class Vowel {
+    public static void main(String args[])
+    {
+        char ch='e';
+        switch(ch)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        System.out.println("vowel");
+        break;
+        default:
+        System.out.println("Constant");
+    }
+    }
+}
