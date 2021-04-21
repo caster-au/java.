84d4a581a797ca9875571abe0a0edace2d4f3aba
caster-au <as0944868@gmail.com>
2021-04-21 12:33:02 +0530

    While loop

---
+++ reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String args[])
+    {
+        int n=153, r,sum=0;
+        while(n!=0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
