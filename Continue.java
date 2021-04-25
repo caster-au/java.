ecd712bd64c003f26361725433076ffa1deaaad0
caster-au <as0944868@gmail.com>
2021-04-25 11:50:24 +0530

    continue

---
+++ Continue.java
@@ -0,0 +1,12 @@
+public class Continue {
+    public static void main(String args[]){
+        int i;
+        for(i=1;i<10;i++){
+            if(i%2==0)
+            {
+                continue;
+            }
+            System.out.println(i+"\t");
+        }
+    }
+}
