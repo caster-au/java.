49410d5218356d43c36c9792642d6aff3bdf703b
caster-au <as0944868@gmail.com>
2021-04-26 09:07:48 +0530

    Nested for loop

---
+++ Nested.java
@@ -0,0 +1,12 @@
+public class Nested {
+    public static void main(String args[])
+    {
+        int i,j;
+        for(i=1;i<3;i++)
+        {
+            for(j=1;j<3;j++)
+            System.out.println(i+" "+j+"\t");
+        }
+        System.out.println();
+    }
+}
